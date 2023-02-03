0a8978cb67448927eacc4eb1ed9cc58746657be3
Ryan Bobrowski <rbobrowski@google.com>
2023-02-03 11:40:07 -0500

    Added remaining utils and events

--- src/my-custom-viz.js
+++ src/my-custom-viz.js
@@ -24,6 +24,7 @@ looker.plugins.visualizations.add({
       display: 'color',
       section: 'Settings',
       order: 2,
+      display_size: 'half',
     },
     numCategories: {
       type: 'number',
@@ -39,19 +40,48 @@ looker.plugins.visualizations.add({
 
   create: function (element, config) {
     css.use()
+
     element.innerHTML = `
-      <h1 id="title" class="title"></h1>
+      <div class="top-bar">
+        <button id="drill-field-example"></button>
+        <h1 id="title" class="title"></h1>
+        <button id="register-option">Register New Option</button>
+      </div>
       <div id="viz-container"></div>
     `
+
+    // Register new option on click
+    document.getElementById('register-option').onclick = () => {
+      this.trigger('registerOptions', {
+        ...this.options,
+        hideTitle: {
+          type: 'boolean',
+          label: 'Hide Title',
+          display: 'radio',
+          default: false,
+          section: 'Settings',
+          order: 3,
+        },
+      })
+    }
   },
 
+  /**
+   * @param data An array of rows representing the current data in the query. May be null.
+   * @param element A DOM Element representing a container to render your visualization into.
+   * @param config An object representing the values of any configuration options that the user has set for this chart.
+   * @param queryResponse An object representing metadata about the current query, such as metadata about fields. May be null.
+   * @param details Details about the current rendering context. Contains information about why the chart is rendering and what has changed. (changed, crossFilterEnabled, crossFilters, print)
+   * @param done A callback to indicate that the visualization is fully rendered. This is especially important to call if your visualization needs to perform asynchronous calls or needs to be rendered as a PDF.
+   * @returns
+   */
   updateAsync: function (data, element, config, queryResponse, details, done) {
     this.clearErrors()
 
-    const { dimension_like, measure_like, table_calculations } = queryResponse.fields
+    const { dimensions, measures, table_calculations } = queryResponse.fields
 
     // Return error if invalid query response
-    if (dimension_like.length != 1 || measure_like.length != 1 || table_calculations.length !== 1) {
+    if (dimensions.length !== 1 || measures.length !== 1 || table_calculations.length !== 1) {
       this.addError({
         title: 'Invalid query response.',
         message: 'One dimension, one measure, and one table calculation are required.',
@@ -61,30 +91,50 @@ looker.plugins.visualizations.add({
 
     // Update title based on config settings
     const title = document.getElementById('title')
-    title.innerHTML = config.title ?? ''
+    title.innerHTML = !config.hideTitle && config.title ? config.title : ''
     title.style.color = config.titleColor ?? '#000000'
 
     // Transform data
     const seriesData = data.slice(0, config.numCategories ?? 10)
-    const categoryFieldName = dimension_like[0].name
+    const categoryFieldName = dimensions[0].name
     const randomFieldName = table_calculations[0].name
-    const incidentFieldName = measure_like[0].name
-    const categories = seriesData.map((d) => d[categoryFieldName].value)
+    const incidentFieldName = measures[0].name
+    const categories = seriesData.map((d) => LookerCharts.Utils.textForCell(d[categoryFieldName]))
     const randomValues = seriesData.map((d) => d[randomFieldName].value)
     const incidents = seriesData.map((d) => d[incidentFieldName].value)
+
+    document.getElementById('drill-field-example').innerHTML = LookerCharts.Utils.htmlForCell(
+      data[0][incidentFieldName]
+    )
 
     // Build chart
     const chart = Highcharts.chart('viz-container', {
       title: '',
       chart: {
         type: 'bar',
+        animation: false,
       },
       xAxis: {
         categories,
       },
       yAxis: {
         title: {
-          text: 'Incidents',
+          text: 'Values',
+        },
+      },
+      plotOptions: {
+        series: {
+          animation: false,
+          events: {
+            click: (e) => {
+              const { index } = e.point
+
+              LookerCharts.Utils.openDrillMenu({
+                links: data[index][incidentFieldName].links,
+                event: e,
+              })
+            },
+          },
         },
       },
       series: [
@@ -97,6 +147,23 @@ looker.plugins.visualizations.add({
           data: incidents,
         },
       ],
+    })
+
+    // Add cross filter functionality to x-axis labels
+    chart.xAxis[0].labelGroup.element.childNodes.forEach((label) => {
+      label.style.cursor = 'pointer'
+      label.onclick = function (e) {
+        const selectedCategory = this.textContent
+        const category = data.filter((d) => d[categoryFieldName].value === selectedCategory)[0][categoryFieldName]
+        // if (details.crossfilterEnabled) {
+        LookerCharts.Utils.toggleCrossfilter({
+          row: {
+            [categoryFieldName]: category,
+          },
+          event: e,
+        })
+        // }
+      }
     })
 
     done()
